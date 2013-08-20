bf336b6ecaab8bf6f672fa702d4e5300a47f2579
Apple <dreamer.x6@gmail.com>
2013-08-21 00:37:39 +0800

    add a line

--- src/com/hello/Hello.java
+++ src/com/hello/Hello.java
@@ -2,6 +2,6 @@ package com.hello;
 
 public class Hello {
 	public static void main(String[] args){
-		System.out.println("Hello GitHub !");
+		System.out.println("Hello GitHub !\nI am zshms.");
 	}
 }
